04abe2d5d16f6d564f0c7abac9da212f54a8dc68
EvaldoAssis <junior29613372@gmail.com>
2024-07-31 00:27:51 -0300

    Uso de estruturas condicionais e repetições

---
+++ Secao04_Estruturas/19_switch/scripts.js
@@ -0,0 +1,19 @@
+let nome = "Otávio";
+
+switch(nome) {
+  case "Otávio":
+    console.log("O Nome é Otávio");
+    break;
+  case "João":
+    console.log("O nome é João");
+    break;
+  default:
+    console.log("O nome não foi encontrado");
+    break;
+}
+
+// if(nome == "Otávio") {
+//   console.log("O Nome é Otávio");
+// } else {
+//   console.log("O nome não foi encontrado");
+// }
