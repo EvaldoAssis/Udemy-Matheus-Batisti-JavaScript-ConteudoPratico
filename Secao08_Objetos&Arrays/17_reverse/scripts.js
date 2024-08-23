1924396408291a0ef50d0f7b142d901c6f834c07
EvaldoAssis <junior29613372@gmail.com>
2024-08-23 00:32:17 -0300

    Aplicação e entendimento de objetos e arrays: método reverse()

---
+++ Secao08_Objetos&Arrays/17_reverse/scripts.js
@@ -0,0 +1,9 @@
+let marcas = ['Adidas', 'Billabong', 'Calvin Klein', 'Diamond', 'Element'];
+
+let numeros = [20, 21, 30, 31, 40, 41, 50];
+
+console.log(marcas);
+console.log(marcas.reverse());
+
+console.log(numeros);
+console.log(numeros.reverse());
