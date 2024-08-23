5e4c1860e5c2f26f2058fbf24dd1a2d04630d67c
EvaldoAssis <junior29613372@gmail.com>
2024-08-23 00:32:06 -0300

    Aplicação e entendimento de objetos e arrays: método includes()

---
+++ Secao08_Objetos&Arrays/16_includes/scripts.js
@@ -0,0 +1,4 @@
+let profissoes = ['Analista de Marketing', 'Programador', 'Pedreiro', 'Marceneiro', 'Uber'];
+
+console.log(profissoes.includes('Uber')); //Retorna true
+console.log(profissoes.includes('Pintor')); //Retorna false
