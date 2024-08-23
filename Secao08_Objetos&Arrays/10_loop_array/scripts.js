8d3d14eb0a94c222a83a303ee4c0408e7bd53f07
EvaldoAssis <junior29613372@gmail.com>
2024-08-22 23:48:12 -0300

    Aplicação e entendimento de objetos e arrays: Loops em Arrays

---
+++ Secao08_Objetos&Arrays/10_loop_array/scripts.js
@@ -0,0 +1,11 @@
+let nomes = ["Tyler", "Tainan", "Thaisa"];
+
+// Loop for
+for(let i = 0; i <= nomes.length; i++) {
+  console.log(nomes[i]);
+}
+
+// Loop foreach
+nomes.forEach(nome => {
+  console.log(nome);
+});
