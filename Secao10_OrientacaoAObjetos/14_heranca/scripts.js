14fc88dd5b1ffc85d83bb8b29c09616366ddb88a
EvaldoAssis <junior29613372@gmail.com>
2024-09-18 00:21:48 -0300

    Correção na chamada de super() no construtor

--- Secao10_OrientacaoAObjetos/14_heranca/scripts.js
+++ Secao10_OrientacaoAObjetos/14_heranca/scripts.js
@@ -11,20 +11,24 @@ let accountOne = new Account(100);
 class CurrentAccount extends Account {
 
   constructor(balance, bank, currency) {
-    super(balance, balance);
+    super(balance);
     this.bank = bank;
     this.currency = currency;
   }
 
   deposit(value) {
     this.balance += value;
-    return this.balance; 
+    return this.balance;
   }
 
 }
 
+let acc = new Account(200);
+console.log(acc);
+
 let accountTwo = new CurrentAccount(200, 'BANCO DE GUAIANASES', 'REAL');
+console.log(accountTwo);
+accountTwo.deposit(100);
+console.log(accountTwo);
 
-console.log(accountTwo.balance);
-
-console.log(accountTwo.deposit(100));
+console.log(acc);
