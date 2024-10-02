cd1a8199257238ac219438142cd340e2deb7cbef
EvaldoAssis <junior29613372@gmail.com>
2024-10-02 20:35:45 -0300

    Revert "Entendendo o uso de conjuntos em expressões regulares (regex)"
    This reverts commit 4d591ef22e9d12b19e5bf8a8649d8af2004a1e78.

--- Secao12_ExpressoesRegulares/02_conjuntos/script.js
+++
@@ -1,9 +0,0 @@
-const reg1 = /[12345]/;
-
-console.log(reg1.test("Temos o número 6")); //false
-console.log(reg1.test("Temos o número 2")); //true
-console.log(reg1.test("Temos o número 26")); //true - O resultado é true porque a expressão regular /[12345]/ verifica se qualquer um dos números 1, 2, 3, 4 ou 5 aparece na string. Mesmo que até esse número o valor requirido for encontrado.
-
-const reg2 = /[1-9]/;
-
-console.log(reg2.test('Temos o número 1234567891')); // true - O regex /[1-9]/ procura por qualquer dígito entre 1 e 9 na string.
