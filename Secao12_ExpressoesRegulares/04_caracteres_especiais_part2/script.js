17118666276c2c62b2be1d1850d69f6697c77410
EvaldoAssis <junior29613372@gmail.com>
2024-10-02 20:34:41 -0300

    Entendendo o uso de conjuntos em expressões regulares (regex)

---
+++ Secao12_ExpressoesRegulares/04_caracteres_especiais_part2/script.js
@@ -0,0 +1,12 @@
+const dia = /\d\d/; //Verificando se a expressão possui dois dígitos númericos consecutivos (uma forma de validar se é um dia)
+
+console.log(dia.test("2019") && "2019".length == 2); //Essa expressão tem o resultado de TRUE, pois por mais que 2019 se trata de um ano, ele possui 2 digitos consecutivos
+console.log(dia.test("asd"));
+console.log(dia.test("05"));
+
+const palavrasPeloMenosTresLetras = /\w\w\w/; //Verificando se a expressão possui ao menos 3 caracteres
+
+console.log('\n');
+console.log(palavrasPeloMenosTresLetras.test("abc"));
+console.log(palavrasPeloMenosTresLetras.test("as"));
+console.log(palavrasPeloMenosTresLetras.test("junior")); //
